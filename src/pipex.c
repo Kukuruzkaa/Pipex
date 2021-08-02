871e3fb382ad2dd1f30b451b0fd8a85010e3c846
daria <dariadiakova@gmail.com>
2021-08-02 19:46:23 +0200

    pipe amd fork

--- src/pipex.c
+++ src/pipex.c
@@ -13,6 +13,10 @@
 #include "pipex.h"
 #include <unistd.h>
 #include <stdio.h>
+#define P_STDIN 0
+#define P_STDOUT 0
+#define P_STDERR 0
+
 
 void		printarray(char *arr)
 {
@@ -30,26 +34,50 @@ int		main(int argc, char **argv, char **envp)
 {
 	// char *getenv;
 	// char **getpath;
-	char **cmd1;
 	// char *tmp;
 	// char *pathname;
+	char **cmd1;
+	//char **cmd2;
+	int		fds[2];
+	pid_t 	childpid;
 	int 	i;
 	(void) envp; 	
 
 	i = 0;
 	// getenv = NULL;
 	// getpath = NULL;
-	// char **cmd2;
+
+	pipe(fds);
 
 	if (!(argc == 5))
 		exit(1);
-	cmd1 = ft_split(argv[2], ' ');
-			// printarray(cmd1[0]);
-		// if (cmd1[1])
-			// printarray(cmd1[1]);
-		// cmd2 = ft_split(argv[2], ' ');
-	// char* arg[] = {"/bin/pwd", 0};
-	
+
+	childpid = fork();
+	if (childpid == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	if (childpid == 0)
+	{
+		write(1, "@", 1);
+		cmd2 = ft_split(argv[3], ' ');
+		dup2(0, fds[0]);
+		execve(cmd2[0], cmd2, NULL);
+		close(fds[1]); // child will read from the pipe, no need to write
+		close(fds[0]);
+	}
+	else 
+	{
+		write(1, "#", 1);
+		cmd1 = ft_split(argv[2], ' ');
+		close(fds[0]); //parent does not need to read
+		dup2(0, fds[1]);
+		execve(cmd1[0], cmd1, NULL);
+		waitpid(childpid, NULL, 0);
+		close(fds[1]);
+	// }
+
 	// while (envp[i])
 	// {
 	// 	if (ft_strnstr(envp[i], "PATH", 4))
@@ -66,6 +94,6 @@ int		main(int argc, char **argv, char **envp)
 	// 	pathname = ft_strjoin(tmp, *cmd1);
 	// 	i++;
 	// }
-	execve(cmd1[0], cmd1, NULL);
+	
 	return 0;
 }
