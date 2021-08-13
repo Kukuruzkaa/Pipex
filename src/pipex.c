1f8b0a6107970a6100d8e4cf446e6d7afdd71f4f
daria <dariadiakova@gmail.com>
2021-08-13 18:08:14 +0200

    out fd oen fixed

--- src/pipex.c
+++ src/pipex.c
@@ -19,17 +19,50 @@
 #define STDERR 2
 
 
-void		printarray(char *arr)
+void 	ft_getpath(char *cmd, char **envp)
 {
-	int length;
+	int i;
+	char **getpath;
+	char *pathname;
+	char *tmp;
 
-	length = ft_strlen(arr);
-	for (int i = 0; i < length; i++)
+	i = 0;
+	getpath = NULL;
+	pathname = NULL;
+
+	while (envp[i])
 	{
-		printf("%c", arr[i]);
+		if (ft_strncmp(envp[i], "PATH", 4) == 0)
+		{
+			envp[i] += 5;
+			getpath = ft_split(envp[i], ':');
+			break;
+		}
+		i++;    
 	}
-	printf("\n");
+	i = 0;
+	while (getpath[i])
+	{
+		tmp = ft_strjoin(getpath[i], "/");
+		pathname = ft_strjoin(tmp, cmd);
+		printf ("cmd %s\n", cmd);
+		printf (" pathname %s\n", pathname);
+		i++;
+	}
+	if (execve(pathname, &cmd, envp) != -1)
+	{
+		free(tmp);
+		free(pathname);
+	}
+	else 
+	{
+		perror("Error");
+		exit(1);
+		free(tmp);
+		free(pathname);
+	}
 }
+
 
 int		main(int argc, char **argv, char **envp)
 {
@@ -39,22 +72,20 @@ int		main(int argc, char **argv, char **envp)
 	pid_t 	childpid1;
 	pid_t	childpid2;
 	int 	status;
-	int 	i;
 	int 	fd_in;
 	int 	fd_out;
 	(void) envp; 	
 
-	i = 0;
 	status = 0;
-	// fd_in = 0;
-	// fd_out = 0;
-	
 
 	if (argc != 5)
 		exit(1);
-
-	cmd1 = ft_split(argv[2], ' ');
-	cmd2 = ft_split(argv[3], ' ');
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in == -1)
+		perror("invalid fd");
+	fd_out = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd_out == -1)
+		perror("invalid fd");
 
 	pipe(fds);
 	if (pipe(fds) == -1)
@@ -71,15 +102,17 @@ int		main(int argc, char **argv, char **envp)
 	if (childpid1 == 0)
 	{
 		// write(1, "@\n", 2);
-		fd_in = open(argv[1], O_RDONLY);
-		if (fd_in == -1)
-			perror("invalid fd");
+		cmd1 = ft_split(argv[2], ' ');
 		close(fds[0]); 
 		dup2(fd_in, 0);
 		close(fd_in);
 		dup2(fds[1], 1);
+		// ft_getpath(*cmd1, envp);
+		if (execve(cmd1[0], cmd1, envp) == -1)
+			exit(1);
+			//ivalid command address
 		close(fds[1]);
-		execve(cmd1[0], cmd1, NULL);
+		// execve(cmd1[0], cmd1, NULL);
 	}
 	else 
 	{
@@ -92,15 +125,17 @@ int		main(int argc, char **argv, char **envp)
 		if (childpid2 == 0)
 		{
 			// write(1, "&\n", 2);
-			fd_out = open(argv[4], O_WRONLY);
-				if (fd_out == -1)
-			perror("invalid fd");
 			close(fds[1]);
+			cmd2 = ft_split(argv[3], ' ');
 			dup2(fd_out, 1);
 			close(fd_out);
 			dup2(fds[0], 0);
+			// ft_getpath(*cmd2, envp);
+			if (execve(cmd2[0], cmd2, envp) == -1)
+				exit(1);
+			//ivalid command address
 			close(fds[0]);
-			execve(cmd2[0], cmd2, NULL);
+			// execve(cmd2[0], cmd2, NULL);
 		}
 		else
 		{		
@@ -111,31 +146,5 @@ int		main(int argc, char **argv, char **envp)
 			waitpid(childpid2, &status, 0);
 		}
 	}
-
-	// char *getenv;
-	// char **getpath;
-	// char *tmp;
-	// char *pathname;
-
-	// getenv = NULL;
-	// getpath = NULL;
-
-	// while (envp[i])
-	// {
-	// 	if (ft_strnstr(envp[i], "PATH", 4))
-	// 	{
-	// 		envp[i] += 5;
-	// 		getpath = ft_split(envp[i], ':');
-	// 	}
-	// 	i++;    
-	// }
-	// i = 0;
-	// while (getpath[i])
-	// {
-	// 	tmp = ft_strjoin(getpath[i], "/");
-	// 	pathname = ft_strjoin(tmp, *cmd1);
-	// 	i++;
-	// }
-	
 	return 0;
 }
